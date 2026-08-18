67c6b30e0987e08a70fba966c3eb0135f4b8df0f
Mac <mac@MacBookPro.lan>
2026-08-18 23:00:58 +0530

    Fix Supplier Restock PO button initialization to default to single size '28' row

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7562,10 +7562,7 @@ return sortedOrders.slice(0, visibleCount)
                                   school: '',
                                   specifications: getDefaultSpecifications(),
                                   sizeBreakdown: [
-                                    { size: '30', orderedQty: '' },
-                                    { size: '32', orderedQty: '' },
-                                    { size: '34', orderedQty: '' },
-                                    { size: '36', orderedQty: '' }
+                                    { size: '28', orderedQty: '' }
                                   ]
                                 }
                               ]
